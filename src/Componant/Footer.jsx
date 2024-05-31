de814bd541bf018a04a617351a6cd96c0c39ec80
Ajit Mote <123333490+Whiterows42@users.noreply.github.com>
2024-05-31 09:55:31 +0530

    footer fixed position removed

--- src/Componant/Footer.jsx
+++ src/Componant/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 py-4 w-full fixed  bottom-0 p-3">
+    <footer className="bg-gray-200 py-4 w-full  p-3">
       <p className="text-sm text-gray-600 flex justify-center ">
         Disclaimer: This website is created for fun and educational purposes
         only. Do not use it to harm or deceive anyone. Always use technology
